aaf42dd704c10d1d1182befbac514d86e64b51d1
mackuz116@gmail.com <mackuz116@gmail.com>
2024-09-25 08:13:27 +0200

    zzz

--- selektor-audio.ino
+++ selektor-audio.ino
@@ -1,7 +1,15 @@
 #include <Wire.h>
 #include <RTClib.h>
 #include <time.h>
+#include <TM1638plus.h> // include the library
 
+#define STROBE_TM 4
+#define CLOCK_TM 6
+#define DIO_TM 7
+
+const long intervalButton = 200;   // interval to read button (milliseconds)
+const long intervalDisplay = 1000; // interval at which to change display (milliseconds)
+bool high_freq = true;             // default false, If using a high freq CPU > ~100 MHZ set to true.
 
 const int change_time = 1000;
 
@@ -10,6 +18,9 @@ char daysOfTheWeek[7][5] = {"NIE", "PON", "WTO", "ŚRO", "CZW", "PIĄ", "SOB"};
 char daysOfTheWeekAll[7][15] = {"Niedziela", "Poniedzialek", "Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota"};
 int pl_week[7] = {1, 2, 3, 4, 5, 6, 0};
 DateTime now;
+
+// Constructor object (GPIO STB , GPIO CLOCK , GPIO DIO, use high freq MCU)
+TM1638plus tm(STROBE_TM, CLOCK_TM, DIO_TM, high_freq);
 
 void setup()
 {
@@ -31,21 +42,56 @@ void setup()
     now = rtc.now();
     Serial.println(now.timestamp(DateTime::TIMESTAMP_TIME));
     Serial.println(now.timestamp(DateTime::TIMESTAMP_DATE));
+
+    tm.displayBegin();
+    tm.displayText("00000000");
+    Serial.begin(38400);
+    delay(100);
+    Serial.println("--Comms UP--TM1638plus_BUTTON_Model1.ino--");
+    delay(5000);
 }
 
 void loop()
 {
 
-  
-  static long last_loop_time = 0;
-  long loop_time = millis();
-  if (loop_time - last_loop_time > change_time)
-  {
-    now = rtc.now();
+    static long last_loop_time = 0;
+    long loop_time = millis();
+    if (loop_time - last_loop_time > change_time)
+    {
+        now = rtc.now();
 
-     last_loop_time = millis();
-  }
+        last_loop_time = millis();
+    }
+    uint8_t buttonsValue = buttonsRead();
+    if (buttonsValue != 0)
+    {
+        updateDisplay(buttonsValue);
+    }
+}
 
+// Read and debounce the buttons from TM1638  every interval
+uint8_t buttonsRead(void)
+{
+    uint8_t buttons = 0;
+    static unsigned long previousMillisButton = 0; // executed once
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillisButton >= intervalButton)
+    {
+        previousMillisButton = currentMillis;
+        buttons = tm.readButtons();
+    }
+    return buttons;
+}
 
- 
+// Change display every interval
+void updateDisplay(uint8_t buttonsValue)
+{
+    unsigned long currentMillis = millis();
+    static unsigned long previousMillisDisplay = 0; // executed once
+    if (currentMillis - previousMillisDisplay >= intervalDisplay)
+    {
+        previousMillisDisplay = currentMillis;
+        tm.displayIntNum(buttonsValue, true, TMAlignTextLeft);
+        Serial.println(buttonsValue, HEX);
+    }
 }
